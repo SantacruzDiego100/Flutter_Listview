0214221990e1a54fe7ee66186d36d0d251bac3a9
DIEGO_LEONARDO_SANTACRUZ_CASTILLO <a.20308051281033@cbtis128.edu.mx>
2023-05-18 21:35:57 +0000

    Flutter_Listview

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class listview2 extends StatelessWidget {
+  var games = ["pow", "fornite", "pacman", "mortal kombat"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('list view2'),
+        ),
+        body: ListView.separated(
+          //
+          itemCount: games.length,
+          itemBuilder: (context, index) => ListTile(
+              title: Text(games[index]),
+              trailing: Icon(Icons.arrow_forward_ios, color: Colors.amber),
+              onTap: () {
+                var seleccion = games[index];
+                print(seleccion);
+              }),
+          separatorBuilder: (_, __) => Divider(),
+        ));
+  }
+}
